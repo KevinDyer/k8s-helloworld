510b2022927f10f87c640d075d032de5086e3d12
Kevin Dyer <kevin.m.dyer@gmail.com>
2017-05-15 07:05:19 -0600

    Fixed bug in gatekeeper-server

--- gatekeeper/server/index.js
+++ gatekeeper/server/index.js
@@ -2,7 +2,7 @@
   'use strict';
   const PORT = process.env.PORT || 80;
   const SocketIOServer = require('socket.io');
-  const io = new SocketIOServer(server);
+  const io = new SocketIOServer();
   io.on('connect', (socket) => {
     console.log(`Socket connected ${socket.id}.`);
     socket.on('disconnect', () => {
